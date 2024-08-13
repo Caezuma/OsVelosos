a8ccc5491047b7d5e366629fab56a2252c8e001e
Felipe Alves <felipe.daconceicao@jala.university>
2024-08-13 11:27:52 -0300

    feat(tests): Add e2e tests for comments with group annotations and refactor performance tests

--- tests/comment/e2e/comment.e2e.test.js
+++ tests/comment/e2e/comment.e2e.test.js
@@ -1,34 +1,36 @@
-require('../../../src/api/core/config/loadEnv');
-const request = require('supertest');
-const app = require('../../../src/app');
-const { createCommentSchema, deleteCommentSchema } = require('../../../src/api/schemas/commentschema');
+require("../../../src/api/core/config/loadEnv");
+const request = require("supertest");
+const app = require("../../../src/app");
+const {
+  createCommentSchema,
+  deleteCommentSchema,
+} = require("../../../src/api/schemas/commentschema");
 
-  /**
-   * @group e2e
-   */
+/**
+ * @group e2e
+ */
 
-describe('Comment E2E Tests', () => {
+describe("Comment E2E Tests", () => {
   const cardId = process.env.CARD_ID;
   let commentId;
 
-
-  test('Create a new comment should return status 201 and commentId', async () => {
+  test("Create a new comment should return status 201 and commentId", async () => {
     const response = await request(app)
       .post(`/trello/cards/${cardId}/comments`)
-      .send({ text: 'E2E Test Comment' })
-      .set('Authorization', `Bearer ${process.env.TOKEN}`);
+      .send({ text: "E2E Test Comment" })
+      .set("Authorization", `Bearer ${process.env.TOKEN}`);
 
     expect(response.status).toBe(201);
-    expect(response.body).toHaveProperty('commentId');
+    expect(response.body).toHaveProperty("commentId");
     const { error } = createCommentSchema.validate(response.body);
     expect(error).toBeUndefined();
     commentId = response.body.commentId;
   });
 
-  test('Delete a comment should return status 200', async () => {
+  test("Delete a comment should return status 200", async () => {
     const response = await request(app)
       .delete(`/trello/cards/${cardId}/comments/${commentId}`)
-      .set('Authorization', `Bearer ${process.env.TOKEN}`);
+      .set("Authorization", `Bearer ${process.env.TOKEN}`);
 
     expect(response.status).toBe(200);
     const { error } = deleteCommentSchema.validate(response.body);
